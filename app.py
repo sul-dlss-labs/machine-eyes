06b2520296c55b3f02947194dfb1cc5163378f84
Jeremy Nelson <jermnelson@gmail.com>
2022-04-25 14:37:27 -0600

    Adds google module and handles different services

--- app.py
+++ app.py
@@ -1,56 +1,68 @@
 """Machine Eyes Streamlit Application"""
 
+from mimetypes import init
 import streamlit as st
+from goog import detect_text
 
 st.title("Machine Eyes")
 
 def drop_file_or_url():
-    header_text = """<h3 style="color:#fe644e">Drop in a handwritten text pdf, jpg, png, or IIIF</h3>"""
-    st.markdown(header_text, unsafe_allow_html=True)
+    inital_page = st.empty()
+    with inital_page.container():
+        header_text = """<h3 style="color:#fe644e">Drop in a handwritten text pdf, jpg, png, or IIIF</h3>"""
+        st.markdown(header_text, unsafe_allow_html=True)
 
-    all_col, goog_col, ms_col, amz_col = st.columns(4)
+        all_col, goog_col, ms_col, amz_col = st.columns(4)
 
-    with all_col:
-        all_services = st.checkbox("All", key="all-services")
+        with all_col:
+            all_services = st.checkbox("All", key="all-services")
 
-    with goog_col:
-        google = st.checkbox("Google", key="google", value=all_services)
+        with goog_col:
+            google = st.checkbox("Google", key="google", value=all_services)
 
-    with ms_col:
-        microsoft = st.checkbox("Microsoft", key="microsoft", value=all_services)
+        with ms_col:
+            microsoft = st.checkbox("Microsoft", key="microsoft", value=all_services)
 
-    with amz_col:
-        amazon = st.checkbox("Amazon", key="amazon", value=all_services)
+        with amz_col:
+            amazon = st.checkbox("Amazon", key="amazon", value=all_services)
 
-    uploaded_file = st.file_uploader("", type=["pdf", "png", "jpg"])
+        uploaded_file = st.file_uploader("", type=["pdf", "png", "jpg"])
 
-    url = st.text_input("Image or IIIF URL")
+        url = st.text_input("Image or IIIF URL")
 
-    if uploaded_file:
-        st.session_state["source"] = uploaded_file
+    if uploaded_file is not None:
+        image_bytes= uploaded_file.getvalue()
+        st.session_state["source"] = image_bytes
+        st.session_state["goog"] = google
+        st.session_state["ms"] = microsoft
+        st.session_state["aws"] = amazon
+        inital_page.empty()
+        show_results(image_bytes)
 
-def show_results():
-    st.header("Google")
-    st.download_button(
-        label="Download transcipt",
-        data='''Google Transcript'''
-    )
+def show_results(image_bytes):
+    
+    if st.session_state["goog"] is True:
+        detect_text(image_bytes)
 
-    st.header("Microsoft")
-    st.download_button(
-        label="Download transcipt",
-        data='''Microsoft Transcript'''
-    )
 
-    st.header("Amazon")
-    st.download_button(
-        label="Download transcipt",
-        data='''Amazon Transcript'''
-    )
+    if st.session_state["ms"] is True:
+        st.header("Microsoft")
+        st.download_button(
+            label="Download transcipt",
+            data='''Microsoft Transcript'''
+        )
 
-if "source" in st.session_state:
-    show_results()
-    # st.session_state.pop("source")
-else:
-    drop_file_or_url()
-    # st.session_state["source"] = "test"
+    if st.session_state["aws"] is True:
+        st.header("Amazon")
+        st.download_button(
+            label="Download transcipt",
+            data='''Amazon Transcript'''
+        )
+
+# if st.session_state.get("source"):
+#     image_bytes = st.session_state.pop("source")
+#     show_results(image_bytes)
+    
+# else:
+drop_file_or_url()
+    # st.session_state["source"] = None
---
+++ goog.py
@@ -0,0 +1,35 @@
+from google.cloud import vision
+import streamlit as st
+
+def detect_text(content):
+    client = vision.ImageAnnotatorClient()
+
+    image = vision.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+
+    header_text = """<h3 style="font-family: Monaco">Google</h3>"""
+    st.markdown(header_text, unsafe_allow_html=True)
+    st.download_button(
+        label="Download transcipt",
+        data='\n'.join([row.description for row in response.text_annotations])
+    )
+
+    image_col, text_col = st.columns(2)
+
+    with image_col:
+        st.image(content)
+
+    with text_col:
+        for page in response.full_text_annotation.pages:
+            for block in page.blocks:
+                st.write(f"Block confidence: {block.confidence}")
+                for paragraph in block.paragraphs:
+                    st.write(f"Paragraph confidence: {paragraph.confidence}")
+                    for word in paragraph.words:
+                        word_text = ''.join([symbol.text for symbol in word.symbols])
+                        st.write(f"Word text: {word_text} (confidence: {word.confidence}")
+
+                        for symbol in word.symbols:
+                            st.write(f"\tSymbol: {symbol.text} (confidence: {symbol.confidence}")
+
